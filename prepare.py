c9e661fd4a90a309e692ce6f15138523bcbd8dc2
XingJin-1 <Xing8428082@gmail.com>
2021-05-25 14:53:08 +0200

    First initial commit

---
+++ prepare.py
@@ -0,0 +1,52 @@
+import requests
+from requests_negotiate_sspi import HttpNegotiateAuth
+import json
+import warnings
+import numpy as np
+import glob, os
+
+warnings.filterwarnings('ignore')
+
+# print("1. Authorizing-------------------")
+# session = requests.session()
+# session.verify = "ca-bundle.crt"
+
+# base_url = 'https://aimlms.muc-gp.icp.infineon.com'
+# project_key= 'AIMLTST2'
+
+# response = session.get(f'{base_url}/v1/oauth/token', auth=HttpNegotiateAuth())
+# print(f'Status: {response.status_code}\nText: {response.text}')
+
+# token = json.loads(response.text)
+
+# headers = {
+#     'accept': 'application/json',
+#     'Authorization': "Bearer {}".format(token['access_token'])
+# }
+# session.headers.update(headers)
+
+# print("2. Uploading-------------------")
+# filename = 'outNew_12May2021141644.mat' # Path to the raw data file to be uploaded
+# metadata = {
+# }
+# files = {
+#     'metadata': (None, json.dumps(metadata), 'application/json'),
+#     'rawDataFile': (filename, open(filename, 'rb'), 'text/plain')
+# }
+ 
+# response = session.post(f'{base_url}/v1/projects/{project_key}/artifacts', files=files)
+ 
+# print('Response Received: ' + str(response.status_code) + ': ' + response.text)
+# dict_out = json.loads(response.text)
+# print(dict_out['artifactID'])
+
+os.chdir("./testDataFolder")
+list_file = [] 
+for file in glob.glob("*.mat"):
+    list_file.append(file)
+
+rep = "REP"
+list_wfm = []
+[list_wfm.append(list_file[i]) for i in range(len(list_file)) if rep in list_file[i]]
+list_general_mat = []
+[list_general_mat.append(list_file[i]) for i in range(len(list_file)) if rep not in list_file[i]]
